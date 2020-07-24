e02bbd4e937b82943716330eb707b568b4f95daa
alerner11 <alyssaplerner@gmail.com>
2020-07-24 13:06:35 -0400

    Initialized, basic foreground scrolling implemented.

---
+++ fruity_bat.rb
@@ -0,0 +1,37 @@
+# TODO: figure out how gemfiles work and get defstruct in there
+
+require 'gosu'
+
+class GameWindow < Gosu::Window
+def initialize(*args)
+    super
+    @images = {
+        background: Gosu::Image.new(self, 'images/background.png', false),
+        foreground: Gosu::Image.new(self, 'images/foreground.png', true)    
+    }
+    @state = {
+        
+    }
+    @scroll_x = 0
+end
+
+    def button_down(button)
+        close if button == Gosu::KbEscape
+    end
+
+    def update
+        @scroll_x += 3
+        if @scroll_x > @images[:foreground].width
+            @scroll_x = 0
+        end
+    end
+
+    def draw
+        @images[:background].draw(0, 0, 0)
+        @images[:foreground].draw(-@scroll_x, 0, 0)
+        @images[:foreground].draw(-@scroll_x + @images[:foreground].width, 0, 0)
+    end
+end
+
+window = GameWindow.new(320, 480, false)
+window.show
